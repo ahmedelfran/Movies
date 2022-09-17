d7e0c528a26182b014dd23887e448caae9900f44
ahmedelfran <78222735+ahmedelfran@users.noreply.github.com>
2022-09-17 21:12:34 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,58 @@
+
+// Swiper
+
+var swiper = new Swiper(".popular-content", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        280:{
+        slidesPerView: 1 ,
+        spaceBetween: 10 ,
+        },
+        320:{
+            slidesPerView: 2 ,
+            spaceBetween: 10 ,
+        },
+        510 :{
+            slidesPerView: 2 ,
+            spaceBetween: 10 ,
+        },
+        758:{
+            slidesPerView: 3 ,
+            spaceBetween: 15 ,
+        },
+        900:{
+            slidesPerView: 4 ,
+            spaceBetween: 20 ,
+        },
+    }
+});
+
+// ---- show-video
+
+let playButtom = document.querySelector('.play-movie');
+let video = document.querySelector('.video-container');
+let videoplay = document.querySelector('#video');
+let closeBtn = document.querySelector('.close-video');
+
+playButtom.onclick = ()=>{
+    video.classList.add("show-video");
+    videoplay.play();
+}
+
+closeBtn.onclick = ()=>{
+    video.classList.remove("show-video");
+    videoplay.pause();
+}
